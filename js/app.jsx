43c48f7b258319fb490c65c24cd3aee361cc3812
m-vdb <mvergerdelbove@work4labs.com>
2016-04-25 19:00:27 -0700

    minimal app

---
+++ js/app.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import CalendarApp from './components/CalendarApp.jsx';
+
+
+ReactDOM.render(
+  <CalendarApp />,
+  document.getElementById('flux-calendar')
+);
---
+++ js/components/CalendarApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export default React.createClass({
+
+  render() {
+    return (
+      <div>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+
+});
